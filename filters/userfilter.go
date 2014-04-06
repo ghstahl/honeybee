35d74cfd4aa4a707b4ecfebf36d9ef046b30dba3
Herb <ghstahl@gmail.com>
2014-04-06 14:03:57 -0700

    back to reflection.

--- filters/userfilter.go
+++ filters/userfilter.go
@@ -4,14 +4,15 @@ import (
 	"github.com/astaxie/beego/context"
 	"fmt"
 	"github.com/astaxie/beego"
-	"github.com/ghstahl/pingbeego/filters"
+	"github.com/ghstahl/pingbeego/reflection"
+	"reflect"
 )
 
-type UserFilterFuncer interface {
-	UserFilterFunc() beego.FilterFunc
+type UserFilterType struct {
 }
-func init() {
-	filters.TheFilterRepository.Repository["UserFilter"] = func(ctx *context.Context) {
+
+func (v UserFilterType) FilterFunc() beego.FilterFunc{
+	return func(ctx *context.Context) {
 		fmt.Println(fmt.Sprintf("ctx.Request.URL: %v",ctx.Request.URL))
 		fmt.Println(fmt.Sprintf("ctx.Request.URL.RequestURI: %v",ctx.Request.URL.RequestURI()))
 		fmt.Println(fmt.Sprintf("ctx.Request.URL.Opaque: %v",ctx.Request.URL.Opaque))
@@ -21,5 +22,11 @@ func init() {
 		fmt.Println(fmt.Sprintf("ctx.Request.URL.Path: %v",ctx.Request.URL.Path))
 		fmt.Println(fmt.Sprintf("ctx.Request.URL.User: %v",ctx.Request.URL.User))
 	}
+}
+
+func init() {
+	var x UserFilterType
+	reflection.TheReflectRepository.ValueRepository["UserFilterType"] = reflect.ValueOf(x)
+	reflection.TheReflectRepository.TypeRepository["UserFilterType"] = reflect.TypeOf(x)
 }
 
