7260e5933c0814867922d4239ca3455256fbd240
Herb <ghstahl@gmail.com>
2014-04-15 11:18:37 -0700

    update

---
+++ controllers/twentyfourtyeight.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"fmt"
+)
+
+type TwentyFourtyEightController struct {
+	beego.Controller
+}
+func (this *TwentyFourtyEightController) Prepare() {
+	fmt.Println("func (c *MainController) Prepare()")
+	this.LayoutSections = make(map[string]string)
+	this.TplNames = "index.tpl"
+	this.LayoutSections["SharedHead"] = "shared/_head.tpl"
+	this.LayoutSections["Header"] = "shared/_header.tpl"
+	this.LayoutSections["Footer"] = "shared/_footer.tpl"
+	this.LayoutSections["HtmlHead"] = ""
+	this.Layout = "shared/_layout.tpl"
+}
+func (this *TwentyFourtyEightController) Get() {
+
+	this.LayoutSections["Script"] = "ngApps/2048/ngScript.tpl"
+
+
+	this.Data["Website"] = "beego.me"
+	this.Data["Email"] = "astaxie@gmail.com"
+	this.Data["Title"] = "2048 Well Hell"
+
+
+	//user := this.Ctx.Request.URL.User
+	//this.Data["LoggedInAs"] = user
+
+
+
+}
