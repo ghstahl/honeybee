718147d625c577130a9a5637d92199347db9ccd7
Herb <ghstahl@gmail.com>
2014-04-16 08:53:02 -0700

    added more angular dependencies

--- static/ngApps/2048/requireBootstrap.js
+++ static/ngApps/2048/requireBootstrap.js
@@ -4,22 +4,37 @@
 require.config({
     baseUrl: '/static/ngApps/2048',
     paths: {
-        'angular': '/static/angular/angular-1.2.16/angular',
-        'angular-resource': '/static/angular/angular-1.2.16/angular-resource',
-        'angular-animate': '/static/angular/angular-1.2.16/angular-animate',
-        'angular-route': '/static/angular/angular-1.2.16/angular-route',
+        'angular': '/static/angular/angular-1.3.0-beta.5/angular',
+        'angular-resource': '/static/angular/angular-1.3.0-beta.5/angular-resource',
+        'angular-cookies': '/static/angular/angular-1.3.0-beta.5/angular-cookies',
+        'angular-touch': '/static/angular/angular-1.3.0-beta.5/angular-touch',
+        'angular-animate': '/static/angular/angular-1.3.0-beta.5/angular-animate',
+        'angular-route': '/static/angular/angular-1.3.0-beta.5/angular-route',
         'bootstrap': '/static/bootstrap.3.1.1/js/bootstrap.min',
         'jquery': '/static/js/jquery-2.1.0.min',
         'angular-ui-bootstrap': '/static/angular/ui-bootstrap-tpls-0.10.0.min'
     },
     shim: {
         'app': {
-            deps: ['angular', 'angular-route', 'angular-resource','bootstrap','angular-ui-bootstrap']
+            deps: ['angular',
+                'angular-route',
+                'angular-resource',
+                'angular-touch',
+                'angular-cookies',
+                'angular-animate',
+                'angular-ui-bootstrap',
+                'bootstrap']
         },
         'angular-route': {
             deps: ['angular']
         },
         'angular-resource': {
+            deps: ['angular']
+        },
+        'angular-touch': {
+            deps: ['angular']
+        },
+        'angular-cookies': {
             deps: ['angular']
         },
         'angular-animate': {
