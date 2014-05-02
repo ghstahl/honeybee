87b6fe7240ff52fe8db6f2411d9c114eaaf81555
ghstahl <ghstahl@gmail.com>
2014-05-02 15:40:55 -0700

    added in angular-couch-potato.js

--- static/ngApps/3/app.js
+++ static/ngApps/3/app.js
@@ -1,23 +1,27 @@
 /**
  * Created by Herb on 3/25/2014.
  */
-define(['routes','services/dependencyResolverFor'], function(config, dependencyResolverFor)
+define(['angular-couch-potato','routes','services/dependencyResolverFor'], function(couchPotato,config, dependencyResolverFor)
 {
     var app = angular.module('app',
         [
             'ngRoute',
             'ui.bootstrap',
+            'scs.couch-potato',
             'ngResource',
             'chieffancypants.loadingBar',
             'ngAnimate',
-            'restangular'
+            'restangular',
+            'ui.router'
         ]);
-
+    couchPotato.configureApp(app);
     app.appGlobal = {
         baseUrl:'/static/ngApps/3/'
     };
     app.config(
         [
+            '$stateProvider',
+            '$urlRouterProvider',
             '$routeProvider',
             '$locationProvider',
             '$controllerProvider',
@@ -26,7 +30,9 @@ define(['routes','services/dependencyResolverFor'], function(config, dependencyR
             '$provide',
 
 
-            function($routeProvider,
+            function($stateProvider,
+                     $urlRouterProvider,
+                     $routeProvider,
                      $locationProvider,
                      $controllerProvider,
                      $compileProvider,
@@ -55,6 +61,22 @@ define(['routes','services/dependencyResolverFor'], function(config, dependencyR
                 }
             }
         ]);
+        app.run(['$couchPotato', '$state', '$stateParams', '$rootScope',
+            function($couchPotato, $state, $stateParams, $rootScope){
+                // by assigning the couchPotato service to the lazy property, we
+                // the register functions will know to run-time-register components
+                // instead of config-time-registering them.
+                app.lazy = $couchPotato;
+
+                // angular-ui-project recommends assigning these services to the root
+                // scope.  Others have argued that doing so can lead to obscured
+                // dependencies and that making services directly available to html and
+                // directives is unclean.  In any case, the ui-router demo assumes these
+                // are available in the DOM, therefore they should be on $rootScope.
+                $rootScope.$state = $state;
+                $rootScope.$stateParams = $stateParams;
+            }])
+
 
     return app;
 });
--- static/ngApps/3/requireBootstrap.js
+++ static/ngApps/3/requireBootstrap.js
@@ -6,10 +6,12 @@ require.config({
     paths: {
         'angular': '/bower_components/angular/angular',
         'angular-resource': '/bower_components/angular-resource/angular-resource',
+        'angular-couch-potato': '/bower_components/angular-couch-potato/dist/angular-couch-potato',
         'angular-cookies': '/bower_components/angular-cookies/angular-cookies',
         'angular-touch': '/bower_components/angular-touch/angular-touch',
         'angular-animate': '/bower_components/angular-animate/angular-animate',
         'angular-route': '/bower_components/angular-route/angular-route',
+        'angular-ui-router': '/bower_components/angular-ui-router/release/angular-ui-router',
         'bootstrap': '/bower_components/bootstrap/dist/js/bootstrap.min',
         'jquery': '/bower_components/jquery/dist/jquery.min',
         'restangular': '/bower_components/restangular/dist/restangular',
@@ -21,6 +23,8 @@ require.config({
         'app': {
             deps: [
                 'angular',
+                'angular-couch-potato',
+                'angular-ui-router',
                 'angular-route',
                 'angular-loading-bar',
                 'angular-resource',
@@ -33,11 +37,17 @@ require.config({
                 'underscore'
             ]
         },
+        'angular-couch-potato': {
+            deps: ['angular-ui-router','angular']
+        },
         'angular-loading-bar': {
             deps: ['angular']
         },
         'restangular': {
             deps: ['angular','underscore']
+        },
+        'angular-ui-router': {
+            deps: ['angular']
         },
         'angular-route': {
             deps: ['angular']
